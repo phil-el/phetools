778c8da0edb4e28b5e3ab0ffa6d52e57391a966d
Philippe Elie <phil.el@free.fr>
2012-01-17 01:49:38 +0000

    don't use hardcoded namespace ; rationalize params order

--- history_credit/get_credit.py
+++ history_credit/get_credit.py
@@ -4,18 +4,38 @@ import MySQLdb
 import os
 import sys
 
-def use_db(conn, name):
-    if name != 'old':
-        q= "use " + name + "wikisource_p;"
+def database_name(domain):
+    if domain in [ '', 'www', 'old' ]:
+        dbname = 'sourceswiki_p'
     else:
-        q = "use sourceswiki_p"
+        dbname = domain + 'wikisource_p'
+    return dbname
+
+def get_source_ns(conn, domain):
+    dbname = database_name(domain)
+
+    cursor = conn.cursor()
+    cursor.execute('use toolserver')
+
+    cursor.execute("""SELECT ns_name, ns_id
+                      FROM namespacename
+                      WHERE dbname = %s AND (ns_name = %s OR ns_name = %s)
+                   """,
+                   [ dbname, 'Page', 'Index' ] )
+    result = {}
+    for r in cursor.fetchall():
+        result[r[0]] = r[1]
+    return result
+
+def use_db(conn, domain):
+    q = 'use ' + database_name(domain)
     cursor = conn.cursor()
     cursor.execute(q)
     return cursor
 
-def prefixed_name(book, cursor, max_count = 5000):
+def prefix_index(cursor, start, ns, max_count = 5000):
     count = 0
-    last = book
+    last = start
     cont = True
     while cont:
         cursor.execute("""SELECT page_title, page_id
@@ -23,17 +43,16 @@ def prefixed_name(book, cursor, max_count = 5000):
                           WHERE page_namespace = %s AND page_title >= %s
                           ORDER BY page_title LIMIT 500
                        """,
-                       # FIXME: do not hardcode 104 ns number of Page:
-                       [ 104, last ])
+                       [ ns, last ])
         for r in cursor.fetchall():
             count += 1
-            if not r[0].startswith(book) or count > max_count:
+            if not r[0].startswith(start) or count > max_count:
                 cont = False
                 break
             last = r[0]
             yield r
 
-def get_revision(rev_ids, cursor):
+def get_revision(cursor, rev_ids):
     rev_id_str = [str(x) for x in rev_ids ]
     q = """SELECT rev_user
            FROM revision
@@ -43,39 +62,40 @@ def get_revision(rev_ids, cursor):
     for r in cursor.fetchall():
         yield r
 
-def get_username(user_ids, cursor):
+def get_username(cursor, user_ids):
     user_id_str = [str(x) for x in user_ids ]
     q = """SELECT user_name, ug_group, user_id
            FROM user, user_groups
-           WHERE user_id IN( %s ) AND ug_user = user_id
+           WHERE user_id IN (%s) AND ug_user = user_id
         """ % (','.join(user_id_str)) 
     cursor.execute(q)
     for r in cursor.fetchall():
         yield r
 
-def get_credit(conn, book, domain = 'fr'):
+def get_credit(conn, domain, book):
     book = book.replace(' ', '_')
+    ns = get_source_ns(conn, domain)
     cursor = use_db(conn, domain)
-
-    rev_ids = [ x[1] for x in prefixed_name(book, cursor) ]
+    rev_ids = [ x[1] for x in prefix_index(cursor, book + '/', ns['Page']) ]
 
     user_ids = {}
-    for x in get_revision(rev_ids, cursor):
+    for x in get_revision(cursor, rev_ids):
         user_ids.setdefault(x[0], 0)
         user_ids[x[0]] += 1
 
     results = {}
-    for x in get_username(user_ids, cursor):
+    for x in get_username(cursor, user_ids):
         results.setdefault(x[0], { 'count' : user_ids[x[2]], 'flags' : [] } )
         results[x[0]]['flags'].append(x[1])
     return results
 
-def create_conn(domain = 'fr'):
+def create_conn(domain):
     conn = MySQLdb.connect(host = "sql-s3", user = "phe",
                            read_default_file=os.path.expanduser("~/.my.cnf"))
     return conn
 
 if __name__ == "__main__":
-    conn = create_conn()
+    domain = 'fr'
+    conn = create_conn(domain)
     for arg in sys.argv[1:]:
-        get_credit(conn, arg)
+        get_credit(conn, domain, arg)
