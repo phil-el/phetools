b83fa9504a2caa6e4981d00cd784f44354b50566
Philippe Elie <phil.el@free.fr>
2013-02-22 13:18:25 +0000

    implement an hocr through djvu and hocr through tesseract job queue

--- hocr/wshocr.py
+++ hocr/wshocr.py
@@ -34,11 +34,22 @@ import hashlib
 import gzip
 import utils
 import signal
+import sys
+sys.path.append("/home/phe/pywikipedia")
+import wikipedia
+import align
+sys.path.append("/home/phe/tools")
+import djvu_text_to_hocr
+import ocr_djvu
+# FIXME: lang to tidy, should be in ocr.py
+import ocrdaemon
 
 mylock = thread.allocate_lock()
 
 E_ERROR = 1
 E_OK = 0
+
+jobs = None
 
 # FIXME: use a real Queue object and avoid polling the queue
 
@@ -70,18 +81,21 @@ def add_job(lock, queue, cmd):
     queue.insert(0, cmd)
     lock.release()
 
-def cache_path(book_name):
+def cache_path(book_name, lang, family):
 
-    book_name = book_name.replace(u'_', u' ')
+    book_name = book_name.replace('_', ' ')
 
-    base_dir  = '/mnt/user-store/phe/cache/hocr/'
+    base_dir  = '/mnt/user-store/phe/cache/hocr/%s/%s/' % (family, lang)
     base_dir += '%s/%s/%s/%s/'
 
     h = hashlib.md5()
-    h.update(book_name.encode('utf-8'))
+    h.update(book_name)
     h = h.hexdigest()
 
     base_dir = base_dir % (h[0:2], h[2:4], h[4:6], h[6:])
+
+    if not os.path.exists(base_dir) and family != 'commons':
+        return cache_path(book_name, 'commons', 'commons')
 
     return base_dir
 
@@ -90,62 +104,145 @@ def ret_val(error, text):
         print "Error: %d, %s" % (error, text)
     return  { 'error' : error, 'text' : text }
 
+def do_hocr_djvu(filename, user, codelang):
+    options = djvu_text_to_hocr.default_options()
+    options.gzip = True
+    path = os.path.split(filename)
+    options.out_dir = path[0] + '/'
+    options.silent = True
+    if djvu_text_to_hocr.parse(options, filename):
+        sha1 = ocr_djvu.sha1(filename)
+        ocr_djvu.write_sha1(sha1, options.out_dir + "sha1.sum")
+        os.remove(filename)
+        return ret_val(E_OK, "do_hocr_djvu() success")
+    else:
+        return ret_val(E_ERROR, "do_hocr_djvu() failure")
+
+def do_hocr_tesseract(filename, user, codelang):
+    # FIXME: we need to recheck here the sha1 checksum, this is because
+    # this request can have be asked twice or more in a row. But what about
+    # if the file changed between the request and now, must we always
+    # reupload the file or better check if the file need to be reuploaded ?
+    options = ocr_djvu.default_options()
+
+    options.silent = False
+    options.gzip = True
+    options.config = 'hocr'
+    # FIXME ?
+    options.num_thread = 2
+    options.lang = ocrdaemon.tesseract_languages.get(codelang, 'eng')
+
+    # FIXME: is changing dir correct ?
+    path = os.path.split(filename)
+    old_cwd = os.getcwd()
+    os.chdir(path[0])
+
+    if ocr_djvu.ocr_djvu(options, path[1]):
+        sha1 = ocr_djvu.sha1(filename)
+        ocr_djvu.write_sha1(sha1)
+        os.remove(filename)
+
+    os.chdir(old_cwd)
+
+    return ret_val(E_OK, "do_hocr_tesseract() finished")
+
+def do_hocr(page, user, codelang):
+    book_name = re.sub('^(.*)/[0-9]+$', '\\1', page)
+
+    site = wikipedia.getSite(code = codelang, fam = 'wikisource')
+    filepage = align.get_filepage(site, unicode(book_name, 'utf-8'))
+    if filepage == None or not filepage.exists():
+        return ret_val(E_ERROR, "wiki page not found")
+
+    path = cache_path(book_name, filepage.site().lang, filepage.site().fam().name)
+    if os.path.exists(path + "sha1.sum"):
+        fd = open(path + "sha1.sum")
+        old_sha1 = fd.read()
+        fd.close()
+        if old_sha1 == sha1:
+            return ret_val(E_OK, "book already hocred")
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    # the job queue processing this item must delete the file once the
+    # job is finished
+    align.copy_file_from_url(filepage.fileUrl(), path + book_name)
+
+    t = time.time()
+
+    if djvu_text_to_hocr.has_word_bbox(path + book_name):
+        jobs['number_of_hocr_djvu_job'] += 1
+        add_job(lock, jobs['hocr_djvu_queue'], (path + book_name, codelang, user, t, None))
+        queue_type = "fast"
+    else:
+        jobs['number_of_hocr_tesseract_job'] += 1
+        add_job(lock, jobs['hocr_tesseract_queue'], (path + book_name, codelang, user, t, None))
+        queue_type = "slow"
+
+    return ret_val(E_OK, "job queued and waiting processing in the %s queue" % queue_type)
+
 def do_get(page, user, codelang):
-    page_nr = re.sub(u'^.*/([0-9]+)$', u'\\1', page)
+    page_nr = re.sub('^.*/([0-9]+)$', '\\1', page)
     try:
         page_nr = int(page_nr)
     except:
         return ret_val(E_ERROR, "unable to extract page number from page=")
 
-    book_name = re.sub(u'^(.*)/[0-9]+$', u'\\1', page)
+    book_name = re.sub('^(.*)/[0-9]+$', '\\1', page)
 
-    base_dir = cache_path(book_name)
+    base_dir = cache_path(book_name, codelang, 'wikisource')
 
     filename = base_dir + 'page_%04d.html' % page_nr
+
+    print "GET:", filename
 
     if os.path.exists(filename + '.gz'):
         fd = gzip.open(filename + '.gz')
     elif os.path.exists(filename):
         fd = open(filename)
     else:
-        return ret_val(E_ERROR, "unable to locate file %s for page %s" % (filename, page.encode('utf-8')))
+        return ret_val(E_ERROR, "unable to locate file %s for page %s" % (filename, page))
 
     text = fd.read()
     fd.close()
 
-    return ret_val(E_OK, text)
+    print "returning data for:", filename
 
-jobs = None
+    return ret_val(E_OK, text)
 
 def html_for_queue(queue):
     html = ''
     for i in queue:
-        mtitle = i[0].decode('utf-8')
-        codelang = i[1]
-        try:
-            msite = wikipedia.getSite(codelang, 'wikisource')
-            page = wikipedia.Page(msite, mtitle)
-            path = msite.nice_get_address(page.urlname())
-            url = '%s://%s%s' % (msite.protocol(), msite.hostname(), path)
-        except:
-            url = ""
-        html += date_s(i[4])+' '+i[2]+" "+i[1]+" <a href=\""+url+"\">"+i[0]+"</a><br/>"
+        html += date_s(i[3])+' '+i[2]+" "+i[1]+" "+i[0]+"<br/>"
     return html
 
 def do_status(lock, conn):
     lock.acquire()
-    m_queue = copy.deepcopy(jobs['get_queue'])
+    get_queue = copy.deepcopy(jobs['get_queue'])
+    hocr_queue = copy.deepcopy(jobs['hocr_queue'])
+    hocr_djvu_queue = copy.deepcopy(jobs['hocr_djvu_queue'])
+    hocr_tesseract_queue = copy.deepcopy(jobs['hocr_tesseract_queue'])
     lock.release()
 
     html = common_html.get_head('hOCR server status')
 
     html += "<body><div>the robot is running.<br/><hr/>"
-    html += "<br/>%d get request queued.<br/>" % len(m_queue)
-    html += html_for_queue(m_queue)
+    html += "<br/>%d get request queued.<br/>" % len(get_queue)
+    html += html_for_queue(get_queue)
+    html += "<br/>%d hocr request queued.<br/>" % len(hocr_queue)
+    html += html_for_queue(hocr_queue)
+    html += "<br/>%d hocr djvu queued.<br/>" % len(hocr_djvu_queue)
+    html += html_for_queue(hocr_djvu_queue)
+    html += "<br/>%d hocr tesseract queued.<br/>" % len(hocr_tesseract_queue)
+    html += html_for_queue(hocr_tesseract_queue)
     html += "<br/>%(number_of_get_job)d get since server start<br/>" % jobs
+    html += "<br/>%(number_of_hocr_job)d hocr since server start<br/>" % jobs
+    html += "<br/>%(number_of_hocr_djvu_job)d djvu hocr since server start<br/>" % jobs
+    html += "<br/>%(number_of_hocr_tesseract_job)d tesseract hocr since server start<br/>" % jobs
     html += '</div></body></html>'
 
-    conn.send(html)
+    conn.sendall(html)
     conn.close()
 
 def stop_queue(queue):
@@ -159,11 +256,15 @@ def stop_queue(queue):
 def on_exit(sign_nr, frame):
         print "STOP"
 
-        # no value to save the get queue but conn must be closed first
+        # no value to save the get queue but we must close conn.
         stop_queue(jobs['get_queue'])
         jobs['get_queue'] = []
 
-        utils.save_obj("wshocr.jobs", jobs)
+        stop_queue(jobs['hocr_queue'])
+        stop_queue(jobs['hocr_djvu_queue'])
+        stop_queue(jobs['hocr_tesseract_queue'])
+
+        utils.save_obj("/home/phe/wshocr.jobs", jobs)
 
 def bot_listening(lock):
 
@@ -210,12 +311,15 @@ def bot_listening(lock):
 
             if cmd == "status":
                 do_status(lock, conn)
+            elif cmd == 'hocr':
+                jobs['number_of_hocr_job'] += 1
+                add_job(lock, jobs['hocr_queue'], (page, lang, user, t, conn))
             elif cmd == "get":
                 jobs['number_of_get_job'] += 1
                 add_job(lock, jobs['get_queue'], (page, lang, user, t, conn))
             else:
                 out = ret_val(E_ERROR, "unknown command: " + cmd)
-                conn.send(json.dumps(out));
+                conn.sendall(json.dumps(out));
                 conn.close()
 
     finally:
@@ -234,34 +338,44 @@ def job_thread(lock, queue, func):
         out = ''
 
         print page, user
-        page = page.decode('utf-8')
-        user = user.decode('utf-8')
         out = func(page, user, codelang)
 
         if conn:
-            conn.send(json.dumps(out))
-            #conn.send(out["text"])
+            conn.sendall(json.dumps(out))
             conn.close()
 
         time2 = time.time()
-        print date_s(time2), page.encode('utf-8'), user.encode("utf8"), codelang, " (%.2f)" % (time2-time1)
+        print date_s(time2), page, user, codelang, " (%.2f)" % (time2-time1)
 
         remove_job(lock, queue)
 
 def default_jobs():
     return {
         'get_queue' : [],
-        'number_of_get_job' : 0
+        'number_of_get_job' : 0,
+        'hocr_queue' : [],
+        'number_of_hocr_job' : 0,
+        'hocr_djvu_queue' : [],
+        'number_of_hocr_djvu_job' : 0,
+        'hocr_tesseract_queue' : [],
+        'number_of_hocr_tesseract_job' : 0,
         }
 
 if __name__ == "__main__":
     # qdel send a SIGUSR2 if -notify is used when starting the job.
     signal.signal(signal.SIGUSR2, on_exit)
     try:
-        jobs = utils.load_obj("wshocr.jobs")
+        jobs = utils.load_obj("/home/phe/wshocr.jobs")
     except:
         jobs = default_jobs()
 
+    # Backward compatibility
+    for key, value in default_jobs().iteritems():
+        jobs.setdefault(key, value)
+
     lock = thread.allocate_lock()
     thread.start_new_thread(job_thread, (lock, jobs['get_queue'], do_get))
+    thread.start_new_thread(job_thread, (lock, jobs['hocr_queue'], do_hocr))
+    thread.start_new_thread(job_thread, (lock, jobs['hocr_djvu_queue'], do_hocr_djvu))
+    thread.start_new_thread(job_thread, (lock, jobs['hocr_tesseract_queue'], do_hocr_tesseract))
     bot_listening(lock)
