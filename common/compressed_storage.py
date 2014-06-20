7656f37500165ad4d74f47ec27ce0a696a62cf04
phil-el <phil.el@free.fr>
2014-06-20 17:23:51 +0200

    Skeleton file, nothing to see.

---
+++ common/compressed_storage.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# @file compressed_storage.py
+#
+# @remark Copyright 2014 Philippe Elie
+# @remark Read the file COPYING
+#
+# @author Philippe Elie
+
+
+if __name__ == "__main__":
+    pass
