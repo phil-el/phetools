be58cc2b7e168a15c86edc6f46eeac1682a85314
Phe <phil.el@free.fr>
2014-08-15 21:04:38 +0000

    Quick and dirty tool to delete all hocr for a given lang. It contains intentionally a syntax error as I don't want to run it accidentally :)

---
+++ hocr/delete_hocr.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# @file delete_hocr.py
+#
+# @remark Copyright 2014 Philippe Elie
+# @remark Read the file COPYING
+#
+# @author Philippe Elie
+
+import os
+import hocr_request
+import hocr
+import db
+
+def delete(bookname, lang):
+    if type(bookname) == type(u''):
+        bookname = bookname.encode('utf-8')
+    bookname = bookname.replace(' ', '_')
+    path = hocr.cache_path(bookname, lang)
+    sha1 = hocr.read_sha1(path)
+
+    db_hocr = hocr_request.DbHocr()
+    with db.connection(db_hocr):
+        q = 'delete from hocr where sha1=%s and lang=%s and title=%s'
+        db_hocr.cursor.execute(q, [sha1, lang, bookname])
+        print db_hocr.cursor.fetchall()
+    if os.path.exists(path + 'sha1.sum'):
+        os.remove(path + 'sha1.sum')
+
+if __name__ == "__main__":
+    db_hocr = hocr_request.DbHocr()
+    db_hocr.open()
+    #q = 'select title, lang from hocr where lang="bn"'
+    db_hocr.cursor.execute(q)
+    for p in db_hocr.cursor.fetchall():
+        delete(p['title'], p['lang'])
+    db_hocr.close()
