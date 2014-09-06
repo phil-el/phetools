0d7327e018388a970a85d5ea53630fa6e89551aa
Phe <phil.el@free.fr>
2014-09-06 20:07:02 +0000

    Encapsulate speller in a class to provide unicode <--> utf-8 conversion here rather than in caller code.

--- spell/spell.py
+++ spell/spell.py
@@ -10,23 +10,30 @@
 import aspell
 import os
 
-def get_speller(lang):
-    if lang in [ 'pt', 'pt_BR' ]:
-        data_dir = os.path.expanduser('~/root/tmp/usr/lib64/aspell')
-    else:
-        data_dir = os.path.expanduser('~/root/usr/lib/aspell')
+class Speller:
+    def __init__(self, lang):
+        if lang in [ 'pt', 'pt_BR' ]:
+            data_dir = os.path.expanduser('~/root/tmp/usr/lib64/aspell')
+        else:
+            data_dir = os.path.expanduser('~/root/usr/lib/aspell')
 
-    speller = aspell.Speller( ( 'data-dir', data_dir ),
-                              ( 'dict-dir', data_dir ),
-                              ( 'size', '80' ),
-                              ( 'sug-mode', 'fast' ),
-                              ( 'encoding', 'utf-8' ),
-                              ( 'lang', lang ) )
-    return speller
+        self.speller = aspell.Speller( ( 'data-dir', data_dir ),
+                                       ( 'dict-dir', data_dir ),
+                                       ( 'size', '80' ),
+                                       ( 'sug-mode', 'fast' ),
+                                       ( 'encoding', 'utf-8' ),
+                                       ( 'lang', lang ) )
+
+    def check(self, word):
+        return True if self.speller.check(word.encode('utf-8')) else False
+
+    def suggest(self, word):
+        suggest = self.speller.suggest(word.encode('utf-8'))
+        return [unicode(x, 'utf-8') for x in suggest]
 
 if __name__ == "__main__":
     import sys
 
-    speller = get_speller(sys.argv[1])
+    speller = Speller(sys.argv[1])
     print speller.check(sys.argv[2])
     print speller.suggest(sys.argv[2])
