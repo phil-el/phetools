68c467ebe72782f6f29bbda422ec4eedcaeb9c84
Phe <phil.el@free.fr>
2015-01-15 17:12:07 +0000

    New tools to queue some ocr related jobs.

---
+++ ocr/queue_build_djvu_from_ocr_rate.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# @file queue_ocr.py
+#
+# @remark Copyright 2014 Philippe Elie
+# @remark Read the file COPYING
+#
+# @author Philippe Elie
+
+import sys
+import os
+sys.path.append(os.path.expanduser('~/phe/jobs'))
+import sge_jobs
+
+def add_request(lang, filenames):
+    job_req = {
+        'jobname' : 'build_djvu',
+        'run_cmd' : 'python',
+        'args' : [
+            os.path.expanduser('build_djvu_from_ocr_rate.py'),
+            '-lang:' + lang,
+            '-only_text', 
+            ],
+        'max_vmem' : 768,
+        }
+
+    for f in filenames:
+        job_req['args'].append(f)
+
+    db_obj = sge_jobs.DbJob()
+
+    print job_req
+
+    db_obj.add_request(**job_req)
+
+def prepare_request(lang, filenames):
+    print "preparing", lang, filenames
+    for f in filenames:
+        if not os.path.exists(f):
+            print >> sys.stderr, "file:", f, "doesn't exist"
+            exit(1)
+    add_request(lang, filenames)
+
+if __name__ == "__main__":
+    filenames = []
+    lang = None
+    for arg in sys.argv[1:]:
+        if arg.startswith('-lang:'):
+            lang = arg[len('-lang:'):]
+        else:
+            filenames.append(arg)
+
+    if not lang:
+        print >> sys.stderr, "-lang: required"
+        exit(1)
+
+    prepare_request(lang, filenames)
---
+++ ocr/queue_correct_ocr.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# @file queue_ocr.py
+#
+# @remark Copyright 2014 Philippe Elie
+# @remark Read the file COPYING
+#
+# @author Philippe Elie
+
+import sys
+import os
+sys.path.append(os.path.expanduser('~/phe/jobs'))
+import sge_jobs
+
+def add_hocr_request(lang, filename):
+    job_req = {
+        'jobname' : 'correct_ocr',
+        'run_cmd' : 'python',
+        'args' : [
+            os.path.expanduser('~/botpywi/correct_ocr.py'),
+            '-lang:' + lang,
+            '-sublang:' + 'fr_18',
+            '-auto',
+            '' + filename
+            ],
+        'max_vmem' : 2048,
+        }
+
+    db_obj = sge_jobs.DbJob()
+
+    print job_req
+
+    db_obj.add_request(**job_req)
+
+def prepare_ocr_request(lang, filename):
+    print "preparing", lang, filename
+    if not os.path.exists(filename):
+        print >> sys.stderr, "file:", filename, "doesn't exist"
+        exit(1)
+    add_hocr_request(lang, filename)
+
+if __name__ == "__main__":
+    filenames = []
+    lang = None
+    for arg in sys.argv[1:]:
+        if arg.startswith('-lang:'):
+            lang = arg[len('-lang:'):]
+        else:
+            filenames.append(arg)
+
+    if not lang:
+        print >> sys.stderr, "-lang: required"
+        exit(1)
+
+    for f in filenames:
+        prepare_ocr_request(lang, f)
